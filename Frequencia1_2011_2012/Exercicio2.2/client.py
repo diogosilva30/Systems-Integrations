8257374c188d3d09fa2517c238b01e345d06f3ff
Diogo Silva <49190578+spamz23@users.noreply.github.com>
2021-05-06 11:36:04 +0100

    Update client.py

--- Frequencia1_2011_2012/Exercicio2.2/client.py
+++ Frequencia1_2011_2012/Exercicio2.2/client.py
@@ -1,4 +1,4 @@
 import requests
 
-resp = requests.post("http://localhost/", data={"op":"2", "aluno":"Silva", "livro": "Sample Book"})
+resp = requests.post("http://localhost/", json={"op":"2", "aluno":"Silva", "livro": "Sample Book"})
 print(resp.text)
